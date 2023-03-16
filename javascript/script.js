903ef710820a015e20cd6740ce7026c2f1269815
Nywaa <nywaavr@gmail.com>
2023-03-16 20:09:07 +0700

    Trying higher order function method

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,41 @@
-//Select Array of button elements
-const wButton = document.querySelectorAll(".soundButton");
+//Select Array of sound button elements
+const soundButton = document.querySelectorAll(".soundButton");
 
 //Test if all the button can be pressed and display an alert
-for (let index = 0; index < wButton.length; index++) {
-    wButton[index].addEventListener("click", buttonTest);
+for (let index = 0; index < soundButton.length; index++) {
+    soundButton[index].addEventListener("click", buttonTest);
 }
 
 //Test function if button is pressed
 function buttonTest()
 {
-    alert("Button Pressed");
+    alert("Button pressed");
 }
+
+//Calculator higher order function challange
+function sum (number1, number2)
+{
+    return number1 + number2;
+}
+
+function subtract (number1, number2)
+{
+    return number1 - number2;
+}
+
+function multiply (number1, number2)
+{
+    return number1 * number2;
+}
+
+function divide (number1, number2)
+{
+    return number1 / number2;
+}
+
+function calculator (number1, number2, operant)
+{
+    return operant(number1, number2);
+}
+
+console.log(calculator(1, 2, multiply));
